0a8a9e183e48cf7ef7cedd630bf0e7cbaf78dfae
cryptokeepto <cryptokeepto@gmail.com>
2020-03-17 16:05:53 +0700

    Init

---
+++ src/components/TaskList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function TaskList() {
+  return <div>tasklist</div>;
+}
+
+export default TaskList;
---
+++ src/components/TaskList.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+import TaskList from "./TaskList";
+
+export default {
+  component: TaskList,
+  title: "TaskList"
+};
+
+export const Default = () => <TaskList />;
